8b2d56f1fe46cb5d12ef901f69cee02b2ca27bf7
CarlosAncapi <115656721+CarlosAncapi@users.noreply.github.com>
2023-01-30 11:22:58 -0300

    useState en formulario

--- src/components/Controlado.jsx
+++ src/components/Controlado.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 
 const Controlado = () => {
 
-    const [title, setTitle ] = useState('');
-    const [description, setDescription ] = useState('');
-    const [state, setState ] = useState('');
+    const [todo,setTodo] = useState({
+        title: 'Todo #01',
+        description: 'Description #01',
+        state: 'pendiente'
+    })
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(title, description, state);
+        console.log(todo.title, todo.description, todo.state);
         
     }
     return (
@@ -18,21 +20,21 @@ const Controlado = () => {
                 placeholder="Igresa todo" 
                 className="form-control mb-2"
                 name="title"
-                value={title}
-                onChange={e => setTitle(e.target.value)}
+                value={todo.title}
+                onChange={e => setTodo({...todo, title: e.target.value})}
             />
             <textarea 
                 className="form-control mb-2" 
                 placeholder="Ingrese descripcion"
                 name="description"
-                value={description}
-                onChange={e => setDescription(e.target.value)}
+                value={todo.description}
+                onChange={e => setTodo({...todo, description: e.target.value})}
             />
             <select 
                 className="form-select mb-2" 
                 name="state"
-                value={state}
-                onChange={e => setState(e.target.value)}
+                value={todo.state}
+                onChange={e => setTodo({...todo, state: e.target.value})}
             >
                     <option  value="pendiente">Pendiente</option>
                     <option  value="completado">Completado</option>
